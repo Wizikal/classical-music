7476c20033eb0f0f0258fe20db08f7d1b46b901b
William Wei <williamw0880@gmail.com>
2018-11-11 20:02:30 -0800

    Basic set up

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 5500
 
 express()
     .use(express.static(path.join(__dirname, 'public')))
-    .get('/', (req, res) => res.send("Hello Leon!"))
+    .get('/', (req, res) => res.render('index.html'))
     .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 
