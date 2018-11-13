6f0c74b655b0e19693aeb20f33fda483cebfbf93
William Wei <williamw0880@gmail.com>
2018-11-12 18:19:39 -0800

    v1

--- public/main.js
+++ public/main.js
@@ -1,3 +1,38 @@
-function buttonClick() {
-    alert("Hello World!")
+// array of amazon s3 song urls
+const playlist = [
+    "https://s3.amazonaws.com/classical-music-wizikal/eine-kleine-nachtmusik-mozart.mp3",
+    "https://s3.amazonaws.com/classical-music-wizikal/fur-elise-beethoven.mp3",
+    "https://s3.amazonaws.com/classical-music-wizikal/moonlight-beethoven.mp3",
+    "https://s3.amazonaws.com/classical-music-wizikal/toccata-and-fugue-in-d-minor-bach.mp3"
+]
+
+// global variables
+var i = 0
+var song = playlist[i]
+var audio = new Audio(song)
+
+audio.addEventListener('ended', () => {
+    if (i >= 2) i = 0
+    else i++
+    song = playlist[i]
+    audio = new Audio(song)
+    audio.play()
+    console.log(`Song ended! Now playing ${song}!`)
+})
+
+// start playing song
+function startMusic() {
+    audio.play()
+    console.log(`Now playing ${song}!`)
 }
+
+// skip current song
+function skipMusic() {
+    if (i >= 2) i = 0
+    else i++
+    song = playlist[i]
+    audio = new Audio(song)
+    audio.play()
+    console.log(`Skipped! Now playing ${song}!`)
+}
+
